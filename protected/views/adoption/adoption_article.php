3490c4568a88bb90c9202cec38487259957e82d5
root <root@localhost.localdomain>
2015-12-09 16:07:36 +0800

    20151209

--- protected/views/adoption/adoption_article.php
+++ protected/views/adoption/adoption_article.php
@@ -1,1 +1,132 @@
-article;
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 col-md-9">
+      <h1 class="page-title05">Real Travelers' Stories You Can Trust</h1>
+      <span>We invite you to read their stories and share in the experiences of the families we have been so fortunate to serve. </span>
+      <div class="pro-detail-block"> <img src="/images/travel-review01.jpg" />
+        <div class="search-travel-reviews">
+          <input id="edit-skeys" class="form-text01" type="text" maxlength="128" size="60" value="" name="skeys" placeholder="Search for Orphanage Name" />
+          <label>IN</label>
+          <select class="form-select01" name="location" id="edit-location">
+            <option value="_none">- Choose Province -</option>
+      		<option> Anhui </option>
+            <option>Beijing</option>
+            <option>Chongqing</option>
+            <option>Fujian</option>
+            <option>Gansu</option>
+            <option>Guangdong</option>
+            <option>Guangxi</option>
+            <option>Guizhou</option>
+            <option>Hebei</option>
+            <option>Heilongjiang</option>
+            <option>Henan</option>
+            <option>Hubei</option>
+            <option>Hunan</option>
+            <option>Jiangsu</option>
+            <option>Jiangxi</option>
+            <option>Jilin</option>
+            <option>Liaoning</option>
+            <option>Ningxia</option>
+            <option>Shaanxi</option>
+            <option>Shandong</option>
+            <option>Shanghai</option>
+            <option>Shanxi</option>
+            <option>Sichuan</option>
+            <option>Xinjiang</option>
+            <option>Yunan</option>
+            <option>Zhejiang</option>
+            <option>Inner Mongolia</option>
+          </select>
+         <input id="edit-submit" class="form-submit02" type="submit" value="Go" name="op" />
+        </div>
+        <div class="travel-reviews-list01">
+        	<div class="reviews-list01-block clearfix">
+            	<div class="group-left">
+                	<img src="/images/reviews-1.jpg" />
+                </div>
+                <div class="group-right">
+                	<h2>2015 Private Visit to Guipin & Nanning SWI</h2>
+                    <p>Amanda is a university professor from Canada. She adopted her daughter Andaya 
+from Guiping SWI in Guangxing province. Now Andaya is nine years old, Amanda 
+asked us to arrange a heritage tour for her family (her daughter Amanda, her son 
+Kian and her mother) from August 8th to 10th asked us to arrange a heritage tour
+for her family (her daughter Amanda, her son to 10th... <a href="#" class="link01">Read more</a></p>
+					<i>Travel Date: Aug 08th, 2015</i>
+                </div>
+            </div>
+            <div class="reviews-list01-block clearfix">
+            	<div class="group-left">
+                	<img src="/images/reviews-1.jpg" />
+                </div>
+                <div class="group-right">
+                	<h2>2015 Private Visit to Guipin & Nanning SWI</h2>
+                    <p>Amanda is a university professor from Canada. She adopted her daughter Andaya 
+from Guiping SWI in Guangxing province. Now Andaya is nine years old, Amanda 
+asked us to arrange a heritage tour for her family (her daughter Amanda, her son 
+Kian and her mother) from August 8th to 10th asked us to arrange a heritage tour
+for her family (her daughter Amanda, her son to 10th... <a href="#" class="link01">Read more</a></p>
+					<i>Travel Date: Aug 08th, 2015</i>
+                </div>
+            </div>
+            <div class="reviews-list01-block clearfix">
+            	<div class="group-left">
+                	<img src="/images/reviews-1.jpg" />
+                </div>
+                <div class="group-right">
+                	<h2>2015 Private Visit to Guipin & Nanning SWI</h2>
+                    <p>Amanda is a university professor from Canada. She adopted her daughter Andaya 
+from Guiping SWI in Guangxing province. Now Andaya is nine years old, Amanda 
+asked us to arrange a heritage tour for her family (her daughter Amanda, her son 
+Kian and her mother) from August 8th to 10th asked us to arrange a heritage tour
+for her family (her daughter Amanda, her son to 10th... <a href="#" class="link01">Read more</a></p>
+					<i>Travel Date: Aug 08th, 2015</i>
+                </div>
+            </div>
+            <div class="reviews-list01-block clearfix">
+            	<div class="group-left">
+                	<img src="/images/reviews-1.jpg" />
+                </div>
+                <div class="group-right">
+                	<h2>2015 Private Visit to Guipin & Nanning SWI</h2>
+                    <p>Amanda is a university professor from Canada. She adopted her daughter Andaya 
+from Guiping SWI in Guangxing province. Now Andaya is nine years old, Amanda 
+asked us to arrange a heritage tour for her family (her daughter Amanda, her son 
+Kian and her mother) from August 8th to 10th asked us to arrange a heritage tour
+for her family (her daughter Amanda, her son to 10th... <a href="#" class="link01">Read more</a></p>
+					<i>Travel Date: Aug 08th, 2015</i>
+                </div>
+            </div>
+            <div class="reviews-list01-block clearfix">
+            	<div class="group-left">
+                	<img src="/images/reviews-1.jpg" />
+                </div>
+                <div class="group-right">
+                	<h2>2015 Private Visit to Guipin & Nanning SWI</h2>
+                    <p>Amanda is a university professor from Canada. She adopted her daughter Andaya 
+from Guiping SWI in Guangxing province. Now Andaya is nine years old, Amanda 
+asked us to arrange a heritage tour for her family (her daughter Amanda, her son 
+Kian and her mother) from August 8th to 10th asked us to arrange a heritage tour
+for her family (her daughter Amanda, her son to 10th... <a href="#" class="link01">Read more</a></p>
+					<i>Travel Date: Aug 08th, 2015</i>
+                </div>
+            </div>
+            <div class="reviews-pagenation" >
+                <ul class="pagination" style="margin: 20px auto;">
+               		<li><a href="#">« first</a></li>
+                    <li><a href="#">&laquo;  previous</a></li>
+                    <li class="active"><a href="#">1</a></li>
+                    <li class="disabled"><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">next  &raquo;</a></li>
+                    <li><a href="#">last »</a></li>
+                </ul>
+            </div>
+            
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-3"> </div>
+  </div>
+</div>
