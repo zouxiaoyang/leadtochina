221f44932aab94b228d0dfaa8b2074152cdc78b1
root <root@localhost.localdomain>
2016-03-17 14:57:56 +0800

    20160317

--- index.php
+++ index.php
@@ -10,5 +10,13 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 ini_set("display_errors","On");
 error_reporting(E_ALL);
+require_once('checkMobile.php');
+$is_mobile_request = is_mobile_request();
+// $is_mobile_request?define('IS_MOBILE',true):define('IS_MOBILE',false);
+if(isset($_GET['m'])&& $_GET['m']==1){
+	define('IS_MOBILE',true);
+}else{
+	define('IS_MOBILE',false);
+}
 require_once($yii);
 Yii::createWebApplication($config)->run();
